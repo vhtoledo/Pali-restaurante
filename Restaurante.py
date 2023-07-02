1852d91178f0ad5a28c9846f235761231183ca4a
victor toledo <querynetworking@gmail.com>
2023-07-01 23:47:11 -0300

    Creacion de los paneles

--- Restaurante.py
+++ Restaurante.py
@@ -15,5 +15,53 @@ aplicacion.title("Pali RestoBar - Sistema de Facturación")
 # Color de fondo de la ventana
 aplicacion.config(bg='DarkGrey')
 
+# Panel superior
+panel_superior = Frame(aplicacion, bd=1, relief=FLAT)
+panel_superior.pack(side=TOP)
+
+# Etiquieta titulo
+etiqueta_titulo = Label(panel_superior, text='Sistema de Facturación', fg='azure4',
+                        font=('Dosis', 48), bg='DarkGrey', width=27)
+etiqueta_titulo.grid(row=0, column=0)
+
+# Panel izquierdo
+panel_izquierdo = Frame(aplicacion, bd=1, relief=FLAT)
+panel_izquierdo.pack(side=LEFT)
+
+# Panel costos
+panel_costos = Frame(panel_izquierdo, bd=1, relief=FLAT)
+panel_costos.pack(side=BOTTOM)
+
+# Panel comidas
+panel_comidas = LabelFrame(panel_izquierdo, text='Comida', font=('Dosis', 19, 'bold'),
+                           bd=1, relief=FLAT, fg='azure4')
+panel_comidas.pack(side=LEFT)
+
+# Panel bebidas
+panel_bebidas = LabelFrame(panel_izquierdo, text='Bebidas', font=('Dosis', 19, 'bold'),
+                           bd=1, relief=FLAT, fg='azure4')
+panel_bebidas.pack(side=LEFT)
+
+# Panel postres
+panel_postres = LabelFrame(panel_izquierdo, text='Postres', font=('Dosis', 19, 'bold'),
+                           bd=1, relief=FLAT, fg='azure4')
+panel_postres.pack(side=LEFT)
+
+# Panel derecha
+panel_derecha = Frame(aplicacion, bd=1, relief=FLAT)
+panel_derecha.pack(side=RIGHT)
+
+# Panel calculadora
+panel_calculadora = Frame(panel_derecha, bd=1, relief=FLAT, bg='DarkGrey')
+panel_calculadora.pack()
+
+# Panel recibo
+panel_recibo = Frame(panel_derecha, bd=1, relief=FLAT, bg='DarkGrey')
+panel_recibo.pack()
+
+# Panel botones
+panel_botones = Frame(panel_derecha, bd=1, relief=FLAT, bg='DarkGrey')
+panel_botones.pack()
+
 # Evitar que la pantalla se cierre
 aplicacion.mainloop()
